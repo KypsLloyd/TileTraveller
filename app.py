12165c03617dfd0e8f575808ef50da153f9ba334
MattiMatt8 <momatthiasson@gmail.com>
2020-09-17 09:42:47 +0000

    Code updated

--- app.py
+++ app.py
@@ -26,10 +26,80 @@ def possible_directions(north, east, south, west):
 
 position = "1,1"
 
+possible_directions(True, False, False, False)
 next_input = input("Direction: ")
 
-if position == "1,1":
-    if next_input.upper() == "N":
-        position = "1,2"
-        possible_directions(True, True, True, False)
-        
+while position != "3,1":
+    if position == "1,1":
+        if next_input.upper() == "N":
+            position = "1,2"
+            possible_directions(True, True, True, False)
+        else:
+            print("Not a valid direction!")
+    elif position == "1,2":
+        if next_input.upper() == "E":
+            position = "2,2"
+            possible_directions(False, False, True, True)
+        elif next_input.upper() == "N":
+            position = "1,3"
+            possible_directions(False, True, False, True)
+        elif next_input.upper() == "S":
+            position = "1,1"
+            possible_directions(True, False, False, False)
+        else:
+            print("Not a valid direction!")
+    elif position == "2,2":
+        if next_input.upper() == "W":
+            position = "1,2"
+            possible_directions(True, True, True, False)
+        elif next_input.upper() == "S":
+            position = "2,1"
+            possible_directions(True, False, False, False)
+        else:
+            print("Not a valid direction!")
+    elif position == "2,1":
+        if next_input.upper() == "N":
+            position = "2,2"
+            possible_directions(False, False, True, True)
+        else:
+            print("Not a valid direction!")
+    elif position == "1,3":
+        if next_input.upper() == "E":
+            position = "2,3"
+            possible_directions(False, True, False, True)
+        elif next_input.upper() == "S":
+            position = "1,2"
+            possible_directions(True, True, True, False)
+        else:
+            print("Not a valid direction!")
+    elif position == "2,3":
+        if next_input.upper() == "E":
+            position = "3,3"
+            possible_directions(False, False, True, True)
+        elif next_input.upper() == "W":
+            position = "1,3"
+            possible_directions(False, True, True, False)
+        else:
+            print("Not a valid direction!")
+    elif position == "3,3":
+        if next_input.upper() == "S":
+            position = "3,2"
+            possible_directions(True, False, True, False)
+        elif next_input.upper() == "W":
+            position = "2,3"
+            possible_directions(False, True, False, True)
+        else:
+            print("Not a valid direction!")
+    elif position == "3,2":
+        if next_input.upper() == "N":
+            position = "3,3"
+            possible_directions(False, False, True, True)
+        elif next_input.upper() == "S":
+            position = "3,1"
+            continue
+        else:
+            print("Not a valid direction!")
+
+    next_input = input("Direction: ")
+else:
+    print("Victory!")
