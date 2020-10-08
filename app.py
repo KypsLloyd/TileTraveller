da7ea6def4e3abe06f0efb0b12b2672270281e77
kyplerespiritu <kypleres312@nemi.tskoli.is>
2020-10-08 10:37:02 +0000

    random seed

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 # Fall sem prentar út allar mögulegar leiðir
 def possible_directions(north, east, south, west): 
     first = False
@@ -22,18 +24,17 @@ def possible_directions(north, east, south, west):
         first = True
     print(".")
 
-def pull_lever(levers_pulled, position):
+def pull_lever(levers_pulled, position, choice):
     if position not in levers_pulled:
-        choice = input("Pull a lever (y/n): ")
-        if choice.lower() == 'y':
+        if choice.lower() == "y":
             levers_pulled.append(position)
             print(f"You received 1 coin, your total is now {len(levers_pulled)}.")
 
 # Fall færir leikmann
-def move(next_input, position, levers_pulled): 
+def move(next_input, position, levers_pulled, choice_lever): 
     if position == "1,1":
         if next_input.upper() == "N":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "1,2"
             possible_directions(True, True, True, False)
         else:
@@ -41,7 +42,7 @@ def move(next_input, position, levers_pulled):
             possible_directions(True, False, False, False)
     elif position == "1,2":
         if next_input.upper() == "E":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "2,2"
             possible_directions(False, False, True, True)
         elif next_input.upper() == "N":
@@ -55,7 +56,7 @@ def move(next_input, position, levers_pulled):
             possible_directions(True, True, True, False)
     elif position == "2,2":
         if next_input.upper() == "W":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "1,2"
             possible_directions(True, True, True, False)
         elif next_input.upper() == "S":
@@ -66,7 +67,7 @@ def move(next_input, position, levers_pulled):
             possible_directions(False, False, True, True)
     elif position == "2,1":
         if next_input.upper() == "N":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "2,2"
             possible_directions(False, False, True, True)
         else:
@@ -74,11 +75,11 @@ def move(next_input, position, levers_pulled):
             possible_directions(True, False, False, False)
     elif position == "1,3":
         if next_input.upper() == "E":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "2,3"
             possible_directions(False, True, False, True)
         elif next_input.upper() == "S":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "1,2"
             possible_directions(True, True, True, False)
         else:
@@ -96,11 +97,11 @@ def move(next_input, position, levers_pulled):
             possible_directions(False, True, False, True)
     elif position == "3,3":
         if next_input.upper() == "S":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "3,2"
             possible_directions(True, False, True, False)
         elif next_input.upper() == "W":
-            pull_lever(levers_pulled, position)
+            pull_lever(levers_pulled, position, choice_lever)
             position = "2,3"
             possible_directions(False, True, False, True)
         else:
@@ -119,13 +120,26 @@ def move(next_input, position, levers_pulled):
     return position, levers_pulled
     
 
-position = "1,1"
+def play():
+    list_of_choice = ["N", "E", "s", "W"]
+    lever_choices = ["y", "n"]
+    choice_lever = int(input("Input seed: "))
+    random.seed(choice_lever)
 
-possible_directions(True, False, False, False)
+    position = "1,1"
+    possible_directions(True, False, False, False)
+    levers_pulled = []
 
-levers_pulled = []
+    while position != "3,1":
+        next_input = random.choice(list_of_choice)
+        next_choice = random.choice(lever_choices)
+        position, levers_pulled = move(next_input, position, levers_pulled, next_choice)
 
-while position != "3,1":
-    next_input = input("Direction: ")
+def main():
+    play_again = "y"
+    while play_again.lower() == "y":
+        play()
+        play_again = input("Play again (y/n): ")
 
-    position, levers_pulled = move(next_input, position, levers_pulled)
+
+main()
