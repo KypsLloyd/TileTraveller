03874943a42ba37b15d96911da77112c821308fe
MattiMatt8 <momatthiasson@gmail.com>
2020-10-08 10:47:23 +0000

    Question 3

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 # Fall sem prentar út allar mögulegar leiðir
 def possible_directions(north, east, south, west): 
     first = False
@@ -42,7 +44,7 @@ def valid_dir_print(pos):
         possible_directions(False, False, True, True)
 
 # Fall uppfærir staðsetningu leikmanns ef það er valid átt
-def valid_move(next_input, coins, position, valid_spots):
+def valid_move(next_input, coins, moves, position, valid_spots):
     lever_positions = [[1,2],[2,2],[2,3],[3,2]]
     next_input = next_input.upper()
     if next_input in valid_spots:
@@ -57,7 +59,7 @@ def valid_move(next_input, coins, position, valid_spots):
         if position in lever_positions:
             coins = pull_lever(coins)
         if position == [3,1]:
-            print(f"Victory! Total coins {coins}.")
+            print(f"Victory! Total coins {coins}. Moves {moves}.")
         else:
             valid_dir_print(position)
         return position, coins
@@ -68,40 +70,46 @@ def valid_move(next_input, coins, position, valid_spots):
 
 # Fall sér um að gefa notanda valmöguleika um að taka í rofa og bætir honum þá við
 def pull_lever(coins):
-    choice = input("Pull a lever (y/n): ")
+    choice = random.choice(["y", "n"])
+    print(f"Pull a lever (y/n): {choice}")
     if choice.lower() == 'y':
         coins += 1
         print(f"You received 1 coin, your total is now {coins}.")
     return coins
 
 # Fall færir leikmann
-def move(next_input, position, coins): 
+def move(next_input, position, coins, moves): 
     if position == [1,1]:
-        position, coins = valid_move(next_input, coins, position, ["N"])
+        position, coins = valid_move(next_input, coins, moves, position, ["N"])
     elif position == [1,2]:
-        position, coins = valid_move(next_input, coins, position, ["E","N","S"])
+        position, coins = valid_move(next_input, coins, moves, position, ["E","N","S"])
     elif position == [1,3]:
-        position, coins = valid_move(next_input, coins, position, ["E","S"])
+        position, coins = valid_move(next_input, coins, moves, position, ["E","S"])
     elif position == [2,1]:
-        position, coins = valid_move(next_input, coins, position, ["N"])
+        position, coins = valid_move(next_input, coins, moves, position, ["N"])
     elif position == [2,2]:
-        position, coins = valid_move(next_input, coins, position, ["W","S"])
+        position, coins = valid_move(next_input, coins, moves, position, ["W","S"])
     elif position == [2,3]:
-        position, coins = valid_move(next_input, coins, position, ["E","W"])
+        position, coins = valid_move(next_input, coins, moves, position, ["E","W"])
     elif position == [3,2]:
-        position, coins = valid_move(next_input, coins, position, ["N","S"])
+        position, coins = valid_move(next_input, coins, moves, position, ["N","S"])
     elif position == [3,3]:
-        position, coins = valid_move(next_input, coins, position, ["S","W"])
+        position, coins = valid_move(next_input, coins, moves, position, ["S","W"])
     return position, coins
 
+# Fall spilar leikinn
 def play():
     position = [1,1]
     coins = 0
+    moves = 0
+    seed = int(input("Input seed: "))
+    random.seed(seed)
     valid_dir_print(position)
-
     while position != [3,1]:
-        next_input = input("Direction: ")
-        position, coins = move(next_input, position, coins)
+        moves += 1
+        next_input = random.choice(["n", "e", "s", "w"])
+        print(f"Direction: {next_input}")
+        position, coins = move(next_input, position, coins, moves)
 
 def main():
     play_again = "y"
